6932c189c53a892df83f34b9c0eab6eedc3d887c
jull128 <yul.perelygina@yandex.ru>
2023-09-07 18:02:27 +0500

    number

--- components/Delivery/Delivery.js
+++ components/Delivery/Delivery.js
@@ -45,15 +45,43 @@ class Delivery {
 </div>
 <div class='delivery__section'>
     <h2>Получатель</h2>
-    <form>
-        <div>
-            <input placeholder='Имя'>
-            <input placeholder='Фамилия' />
+    <form class='form'>
+        <div class='form_line'>
+        <div class='form_input_container'>
+            <div class="Input">
+                <input type="text" id="input" class="Input-text" placeholder="Имя">
+                <label for="input" class="Input-label">Имя</label>
+            </div>
+
         </div>
-        <div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div class='form_input_container'>
+            <div class="Input">
+                <input type="text" id="input" class="Input-text" placeholder="Фамилия">
+                <label for="input" class="Input-label">Фамилия</label>
+
+            </div>
+        </div>
+      </div>
+      <div class='form_line'>
+      <div class='form_input_container'>
+      <div class="Input">
+          <input type="text" id="input" class="Input-text" placeholder="Почта">
+          <label for="input" class="Input-label">Почта</label>
+      </div>
+  </div>
+  <div class='form_input_container'>
+      <div class="Input">
+          <input type="text" id="tel" onkeydown='phoneNumberFormatter()' class="Input-text tel" placeholder="Телефон">
+          <label for="input"  class="Input-label">Телефон</label>
+      </div>
+  </div>
+  <div class='form_input_container'>
+  <div class="Input">
+      <input type="text" id="input" class="Input-text" placeholder="ИНН">
+      <label for="input" class="Input-label">ИНН</label>
+  </div>
+</div>
+        </div>
         </div>
     </form>
 </div>
@@ -69,3 +97,43 @@ class Delivery {
 
 const deliveryPage = new Delivery();
 deliveryPage.render();
+
+// const createPhone = (str) => str.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+
+const createPhone = (v) => {
+  if (!v) return v;
+  const phoneNumber = v.replace(/[^\d]/g, "");
+  const phoneNumberLength = phoneNumber.length;
+  if (phoneNumberLength < 2) {
+    return `${phoneNumber.slice(0, 1)}`;
+  }
+  if (phoneNumberLength < 4) {
+    return `${phoneNumber.slice(0, 1)} ${phoneNumber.slice(1)}`;
+  }
+  if (phoneNumberLength < 7) {
+    return `${phoneNumber.slice(0, 1)} ${phoneNumber.slice(
+      1,
+      4
+    )} ${phoneNumber.slice(4, 7)}`;
+  }
+  if (phoneNumberLength < 9) {
+    return `${phoneNumber.slice(0, 1)} ${phoneNumber.slice(
+      1,
+      4
+    )} ${phoneNumber.slice(4, 7)}-${phoneNumber.slice(7, 9)}`;
+  }
+
+  return `${phoneNumber.slice(0, 1)} ${phoneNumber.slice(
+    1,
+    4
+  )} ${phoneNumber.slice(4, 7)}-${phoneNumber.slice(7, 9)}-${phoneNumber.slice(
+    9,
+    11
+  )}`;
+};
+
+const phoneNumberFormatter = () => {
+  const inputField = document.getElementById("tel");
+  const formattedInputValue = createPhone(inputField.value);
+  inputField.value = formattedInputValue;
+};
